f6577d3dfc1aa5c816f70f53f8389e5fbd24756b
Mahesh Umale <maheshumale55@gmail.com>
2023-11-24 12:13:57 +0530

    add task functionality2

--- todo/urls.py
+++ todo/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    
+    completed_tasks = Task.objects.filter(is_completed=True)
+    # print(completed_tasks)
+
     context = {
         'tasks' : tasks,
+        'completed_tasks': completed_tasks,
+
     }
     return render(request, 'home.html', context)
